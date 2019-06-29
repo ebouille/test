9a1bfc079bc7338f48e316ebc59107ebb49a8448
ebouille <ebouille@purdue.edu>
2019-06-29 19:26:15 -0400

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # test
 This is just a test repository
+
+# What it does
+It does next to nothing
--- test.py
+++ test.py
@@ -3,3 +3,12 @@ import pandas as pd
 import sklearn
 
 print('This is just a test')
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+x = [1,2,3,4,5]
+y = [1,2,3,4,5]
+
+plt.plot(x,y)
+plt.show()
