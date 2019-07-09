97e0b71ce9f34e8ddfc616fb8a1a33f93cca9443
ebouille <ebouille@purdue.edu>
2019-07-09 00:04:52 -0400

    This is today's commit...

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import sklearn
+import tensorflow as tf
 
 print('This is just a test')
 
@@ -9,6 +10,9 @@ import matplotlib.pyplot as plt
 
 x = [1,2,3,4,5]
 y = [1,2,3,4,5]
+
+# this is a new comment for testing
+
 
 plt.plot(x,y)
 plt.show()
